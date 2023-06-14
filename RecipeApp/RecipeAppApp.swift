e6f883017be0822b7976f748547071bdc002d4a9
Wilson Kerk <wilson.kerk@healint.com>
2023-06-14 17:26:13 +0800

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Wilson Kerk on 14/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
